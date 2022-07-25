7bce19152a1ee5171cea13981d51d054fdec31da
BasantaBabu <basantab.ghimire@gmail.com>
2022-07-25 13:27:05 -0400

    1st commit set all enviroment

---
+++ src/main/java/com/example/autographEV/AutographEvApplication.java
@@ -0,0 +1,13 @@
+package com.example.autographEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutographEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutographEvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autographEV/AutographEvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autographEV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutographEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
