e41a467b88bed15e16076ef3d42c39b08b8382f9
BasantaBabu <basantab.ghimire@gmail.com>
2022-10-18 14:49:13 -0400

    commit firebase auth

--- src/main/java/com/example/autographEV/firebase/FirebaseService.java
+++ src/main/java/com/example/autographEV/firebase/FirebaseService.java
@@ -1,2 +1,41 @@
-package com.example.autographEV.firebase;public class FirebaseService {
+package com.example.autographEV.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+@Service
+public class FirebaseService {
+    public FirebaseApp initializeFirebase() throws IOException {
+        PathMatchingResourcePatternResolver resolver= new PathMatchingResourcePatternResolver();
+        Resource resource =resolver.getResource("classpath:login-backend-spring.json");
+
+        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://login-backend-spring.firebaseio.com/")
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+
+    }
+    public FirebaseUser authenticate(String idToken) throws IOException, FirebaseAuthException {
+
+       String uid = FirebaseAuth.getInstance(initializeFirebase()).verifyIdToken(idToken).getUid();
+       String name = FirebaseAuth.getInstance(initializeFirebase()).verifyIdToken(idToken).getName();
+       String email = FirebaseAuth.getInstance(initializeFirebase()).verifyIdToken(idToken).getEmail();
+
+       return new FirebaseUser(uid, name, email);
+
+    }
 }
--- src/main/java/com/example/autographEV/user/UserController.java
+++ src/main/java/com/example/autographEV/user/UserController.java
@@ -1,12 +1,16 @@
 package com.example.autographEV.user;
 
 import com.example.autographEV.exception.RestrictedInfoException;
+import com.example.autographEV.firebase.FirebaseService;
+import com.example.autographEV.firebase.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -17,10 +21,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private FirebaseService firebaseService;
+
     @PostMapping
-    public User saveUser(@RequestBody @Valid User user){
-        System.out.println("Add user info");
-        return userService.saveUser(user);
+    public User saveUser(@RequestBody @Valid User user, @RequestHeader(name = "idToken") String idToken) throws IOException, FirebaseAuthException {
+        //System.out.println("Add user info");
+        FirebaseUser firebaseUser = firebaseService.authenticate(idToken);
+        if(firebaseUser != null){
+            return userService.saveUser(user);
+        }
+        return null;
     }
 
     @GetMapping
@@ -28,6 +39,7 @@ public class UserController {
         System.out.println("All user information");
         return userService.getAllUsers();
     }
+
     @GetMapping("/{userId}")
     public User getById(@RequestParam("userId") String userId) {
         System.out.println("One User information get based by the given ID");
@@ -45,11 +57,13 @@ public class UserController {
         System.out.println("Delete an user information by Id" );
         userService.deleteUser(userId);
     }
+
     @GetMapping("/{address}")
     public List<User> getByAddress(@RequestParam("address") String address){
         System.out.println("Get user information by address");
         return userService.getByAddress(address);
     }
+
     @GetMapping("/{name}")
     public List<User>getByName(@RequestParam("name") String name) throws RestrictedInfoException{
         if(name.equalsIgnoreCase("root")){
